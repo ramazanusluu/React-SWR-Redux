6dd13c97c1a34a3d77e9edb8a42a3b33c1acd8e7
Ramazan USLU <ruslu1720@gmail.com>
2022-09-09 22:29:59 +0300

    Header component'i olsuturuldu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
+import headerLogo from "../../image/header-logo.png";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="my-3">
+      <ul className="nav justify-content-center">
+        <li className="nav-item">
+          <Link to="/">
+            <img
+              src={headerLogo}
+              alt="logo"
+              title="vestel-logo"
+              style={{ width: "140px" }}
+            />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
