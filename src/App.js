38cb3b2b2211b5b2a4fc6b42c6ea4babf57cef26
Ramazan USLU <ruslu1720@gmail.com>
2022-09-09 22:44:07 +0300

    Categories page component'i olusturuldu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import Categories from "./pages/Categories/Categories";
 
 function App() {
   return (
     <>
       <Header />
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Categories />} />
+      </Routes>
     </>
   );
 }
