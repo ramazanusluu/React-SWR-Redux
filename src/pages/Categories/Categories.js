49ea0ad8a92c1612e46a142290bdfeb142294cd7
Ramazan USLU <ruslu1720@gmail.com>
2022-09-10 08:20:45 +0300

    axios eklendi

--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,6 +1,30 @@
 import React from "react";
+import Loading from "../../components/Loading/Loading";
+import useSWR from "swr";
+import axios from "axios";
+
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 function Categories() {
+  const { data, isValidating, error } = useSWR(
+    "https://store.vrunibex.com/mobile2/mbProduct/CategoryList",
+    fetcher,
+    {
+      revalidateOnFocus: false,
+      revalidateIfStale: false,
+    }
+  );
+  if (error) return <div>failed to load</div>;
+  if (!data)
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  console.log(data);
+  console.log("isValidating", isValidating);
+  //isValidating veriyi yakalama islemi devam ederken true tamamlandığında false olur.
   return (
     <div>
       <h1>Category List</h1>
